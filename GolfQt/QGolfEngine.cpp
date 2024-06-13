1e38feeac7355890615b3ee0b22066fc3849f31f
Gabriel Previato <gabriel.previato@gmail.com>
2024-06-13 23:15:47 +0200

    Added future object

--- GolfQt/QGolfEngine.cpp
+++ GolfQt/QGolfEngine.cpp
@@ -22,23 +22,33 @@ QGolfEngine::~QGolfEngine() {
 
 void QGolfEngine::run_tick() {
     for (auto& o : m_objects) {
-        if (std::abs(o.speed.x) > Physics::SIGMA || std::abs(o.speed.y) > Physics::SIGMA) {
-            Vec2d friction = (o.speed * (-1.0)).unit();
-            o.tick(friction);
-            qDebug() << "o speed is: " << o.speed.x << ", " << o.speed.y;
-            qDebug() << "o friction is: " << friction.x << ", " << friction.y;
+		Physics::Object future_o = o;
+
+        if (std::abs(future_o.speed.x) > Physics::SIGMA || std::abs(future_o.speed.y) > Physics::SIGMA) {
+            Vec2d friction = (future_o.speed * (-1.0)).unit();
+            future_o.tick(friction);
+            qDebug() << "future_o speed is: " << future_o.speed.x << ", " << future_o.speed.y;
+            qDebug() << "future_o friction is: " << friction.x << ", " << friction.y;
         }
         else {
-            o.tick(Vec2d(0, 0));
+            future_o.tick(Vec2d(0, 0));
         }
         //qDebug() << "o position x is: " << o.position.x ;
         //qDebug() << "o speed x is: " << o.speed.x;
-    }
+        auto possible_collisions = m_hash.broad_collision(future_o);
+        qDebug() << "Possbile collision objects: " << possible_collisions.size();
+        for (auto& wall : possible_collisions) {
+            wall->set_color(QColor(255, 0, 0, 127));
+        }
 
-    auto possible_collisions = m_hash.broad_collision(m_objects[0]);
-    qDebug() << "Possbile collision objects: " << possible_collisions.size();
-    for (auto& wall : possible_collisions) {
-        wall->set_color(QColor(255, 0, 0, 127));
+		if (possible_collisions.size() > 0) {
+			o.speed = Vec2d(0, 0);
+		}
+		else {
+			o.speed = future_o.speed;
+			o.position = future_o.position;
+		}
+
     }
 
     emit walls(m_walls);
