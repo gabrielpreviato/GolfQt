0d20276d3e9f34bf83a6caa5b7bdc1cc2eaac170
Gabriel Previato <gabriel.previato@gmail.com>
2024-06-13 23:49:43 +0200

    Added projection method

--- GolfQt/Structure.hpp
+++ GolfQt/Structure.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <algorithm>
 #include <cmath>
 #include <utility>
 
@@ -8,6 +9,7 @@
 #include "Physics-Constants.hpp"
 #include "Vec2.hpp"
 #include "Line2.hpp"
+#include <qDebug>
 
 namespace Physics {
 
@@ -21,6 +23,25 @@ namespace Physics {
 
 		void set_color(QColor new_color) {
 			bg_color = new_color;
+		}
+
+		bool projections_overlap(const Structure& other) {
+			auto min_x = std::min({ v1.x, v2.x, v3.x, v4.x });
+			auto min_y = std::min({ v1.y, v2.y, v3.y, v4.y });
+			auto max_x = std::max({ v1.x, v2.x, v3.x, v4.x });
+			auto max_y = std::max({ v1.y, v2.y, v3.y, v4.y });
+
+			auto other_min_x = std::min({ other.v1.x, other.v2.x, other.v3.x, other.v4.x });
+			auto other_min_y = std::min({ other.v1.y, other.v2.y, other.v3.y, other.v4.y });
+			auto other_max_x = std::max({ other.v1.x, other.v2.x, other.v3.x, other.v4.x });
+			auto other_max_y = std::max({ other.v1.y, other.v2.y, other.v3.y, other.v4.y });
+
+			bool x_overlap = (min_x <= other_max_x && max_x >= other_min_x);
+			bool y_overlap = (min_y <= other_max_y && max_y >= other_min_y);
+
+			qDebug() << "X overlap: " << x_overlap << "; Y overlap: " << y_overlap;
+
+			return x_overlap && y_overlap;
 		}
 	};
 }
